1e3c8db3435361efc126b8cbce4fc28845dc3bf6
DESKTOP-MTL7LKS\Clay <claybeaver1991@gmail.com>
2020-08-12 10:41:32 -0500

    Table data appended to web page

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -1,4 +1,23 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Variable for table body
+var tbody = d3.select('tbody');
+
+// Console.log the ufo data from data.js
+console.log(data);
+
+// Functions 
+function createTable(ufoData) {
+    tbody.html('');
+    ufoData.forEach(item => {
+        var newRow = tbody.append('tr');
+        Object.values(item).forEach(value => {
+            var cell = newRow.append('td');
+            cell.text(value);
+        });
+    });
+}
+
+
+createTable(tableData);

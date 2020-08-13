dc538842fc51a2f6bfe5a583eb3ee3aea8eef585
DESKTOP-MTL7LKS\Clay <claybeaver1991@gmail.com>
2020-08-13 14:07:59 -0500

    UFO-level-2 Completed

--- UFO-level-2/static/js/app.js
+++ UFO-level-2/static/js/app.js
@@ -1,4 +1,56 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Variable for table body
+var tbody = d3.select('tbody');
+
+// Console.log the ufo data from data.js
+console.log(data);
+
+// Create table function
+function createTable(ufoData) {
+    tbody.html('');
+    ufoData.forEach(sighting => {
+        var newRow = tbody.append('tr');
+        Object.values(sighting).forEach(value => {
+            var cell = newRow.append('td');
+            cell.text(value);
+        });
+    });
+}
+
+createTable(tableData);
+
+// variables
+var button = d3.select('#filter-btn')
+var input = d3.select('#search-criteria')
+
+// Create search / filter function that will search 5 columns
+function searchFilter() {
+    d3.event.preventDefault();
+    console.log('You clicked the "Filter Table" button!');
+    var searchInput = d3.select('#search-criteria').property('value');
+    var filterTable = tableData.filter(item => {
+        return (
+            item.datetime === searchInput ||
+            item.city === searchInput ||
+            item.state === searchInput ||
+            item.country === searchInput ||
+            item.shape === searchInput
+        )
+
+    });
+    createTable(filterTable)
+}
+
+function handleKeypress() {
+    if (d3.event.keyCode === 13) {
+        d3.event.preventDefault();
+        console.log(d3.event)
+        searchFilter()
+    }
+}
+
+// Event for click and keypress enter
+d3.select('body').on('keypress', handleKeypress)
+button.on('click', searchFilter)
